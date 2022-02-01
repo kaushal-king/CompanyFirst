b81ac9b9d78ac49e0a7dc4f60802fa390cdb37c7
hp <priyanshudesai9574@gmail.com>
2022-02-01 15:51:32 +0530

    Optimized Code with Updated Currency Converter from Radio Button to TextList

--- app/src/main/java/com/the/firsttask/ui/converter/ConverterFragment.kt
+++ app/src/main/java/com/the/firsttask/ui/converter/ConverterFragment.kt
@@ -3,13 +3,12 @@ package com.the.firsttask.ui.converter
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.the.firsttask.R
 import com.the.firsttask.databinding.FragmentConverterBinding
 
@@ -18,15 +17,10 @@ class ConverterFragment : Fragment() {
 
 
     private var _binding: FragmentConverterBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
-//    lateinit var radioGroup: RadioGroup
     lateinit var editText: EditText
     lateinit var answer: TextView
-    var selected: Int = 0
     var amounttype: String = ""
 
     override fun onCreateView(
@@ -39,27 +33,11 @@ class ConverterFragment : Fragment() {
         _binding = FragmentConverterBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-//        radioGroup = binding.radiogroup
         editText = binding.edAmount
         answer = binding.txtAllAns
 
-
-//        radioGroup.setOnCheckedChangeListener(
-//            RadioGroup.OnCheckedChangeListener { group, checkedid ->
-//                val radio: RadioButton = root.findViewById(checkedid)
-//                Toast.makeText(
-//                    context, " On checked change :" +
-//                            " ${radio.text}",
-//                    Toast.LENGTH_SHORT
-//                ).show()
-//                calculateamount(amounttype)
-//
-//            }
-//        )
-
         editText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
             }
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
@@ -70,20 +48,9 @@ class ConverterFragment : Fragment() {
             }
 
             override fun afterTextChanged(p0: Editable?) {
-
             }
 
-        }
-
-        )
-
-
-//        val textView: TextView = binding.textHome
-//        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-//            textView.text = it
-//        })
-//
-
+        })
         return root
     }
 
